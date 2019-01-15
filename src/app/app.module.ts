696720ec3b976ebb0e6363e56a1fc6fb2dbf8257
matthiasunt <17273057+matthiasunt@users.noreply.github.com>
2019-01-15 16:54:07 +0100

    Fix injection issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,25 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {RouteReuseStrategy} from '@angular/router';
-import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
 import {SplashScreen} from '@ionic-native/splash-screen/ngx';
 import {StatusBar} from '@ionic-native/status-bar/ngx';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
 
 @NgModule({
     declarations: [AppComponent],
     entryComponents: [],
     imports: [
         BrowserModule,
-        HttpClientModule,
         IonicModule.forRoot(),
+        AppRoutingModule,
+        HttpClientModule,
         TranslateModule.forRoot({
             loader: {
                 provide: TranslateLoader,
@@ -25,7 +27,6 @@ import {TranslateHttpLoader} from '@ngx-translate/http-loader';
                 deps: [HttpClient]
             }
         }),
-        AppRoutingModule
     ],
     providers: [
         StatusBar,
