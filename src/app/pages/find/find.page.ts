8c53c455bd42093ec1cff5ba5f15ed6ee840f27c
matthiasunt <17273057+matthiasunt@users.noreply.github.com>
2019-05-13 09:44:41 +0200

    Refactor districts constant

--- src/app/pages/find/find.page.ts
+++ src/app/pages/find/find.page.ts
@@ -15,6 +15,7 @@ import {District} from '@models/district';
 import {Shuttle} from '@models/shuttle';
 import {ENV} from '@env';
 import {ListElement} from '@models/list-element';
+import {Districts} from '../../../assets/data/districts';
 
 @Component({
     selector: 'app-find',
@@ -27,7 +28,7 @@ export class FindPage implements OnInit {
     favorites: Shuttle[];
 
     lang: string;
-    districts: District[] = districts;
+    districts: District[] = Districts;
 
     constructor(private zone: NgZone,
                 private navCtrl: NavController,
@@ -111,175 +112,4 @@ export class FindPage implements OnInit {
         await alert.present();
     }
 }
-
-const districts = [
-    {
-        'type': 'district',
-        'name': {
-            'de': 'Bozen & Umgebung',
-            'it': 'Bolzano e dintorni',
-            'de_st': 'Boazen & Umgebung'
-        },
-        'region': {
-            'de': 'Südtirol',
-            'it': 'Alto Adige'
-        },
-        'country': {
-            'de': 'Italien',
-            'it': 'Italia'
-        },
-        'coordinates': {
-            'latitude': 46.501696,
-            'longitude': 11.350951
-        },
-        '_id': 'italien-suedtirol-bozenumgebung',
-    },
-    {
-        'type': 'district',
-        'name': {
-            'de': 'Burggrafenamt',
-            'it': 'Burgraviato',
-            'de_st': 'Burggrofenomt'
-        },
-        'region': {
-            'de': 'Südtirol',
-            'it': 'Alto Adige'
-        },
-        'country': {
-            'de': 'Italien',
-            'it': 'Italia'
-        },
-        'coordinates': {
-            'latitude': 46.669695,
-            'longitude': 11.144957
-        },
-        '_id': 'italien-suedtirol-burggrafenamt',
-    },
-    {
-        'type': 'district',
-        'name': {
-            'de': 'Eisacktal',
-            'it': 'Valle Isarco',
-            'de_st': 'Eisocktol'
-        },
-        'region': {
-            'de': 'Südtirol',
-            'it': 'Alto Adige'
-        },
-        'country': {
-            'de': 'Italien',
-            'it': 'Italia'
-        },
-        'coordinates': {
-            'latitude': 46.711143,
-            'longitude': 11.658568
-        },
-        '_id': 'italien-suedtirol-eisacktal',
-    },
-    {
-        'type': 'district',
-        'name': {
-            'de': 'Pustertal',
-            'it': 'Val Pusteria',
-            'de_st': 'Pustotol'
-        },
-        'region': {
-            'de': 'Südtirol',
-            'it': 'Alto Adige'
-        },
-        'country': {
-            'de': 'Italien',
-            'it': 'Italia'
-        },
-        'coordinates': {
-            'latitude': 46.799584,
-            'longitude': 11.951079
-        },
-        '_id': 'italien-suedtirol-pustertal',
-    },
-    {
-        'type': 'district',
-        'name': {
-            'de': 'Schlerngebiet - Gröden',
-            'it': 'Altopiano dello Sciliar - Val Gardena',
-            'de_st': 'Schlerngebiet - Greden'
-        },
-        'region': {
-            'de': 'Südtirol',
-            'it': 'Alto Adige'
-        },
-        'country': {
-            'de': 'Italien',
-            'it': 'Italia'
-        },
-        'coordinates': {
-            'latitude': 46.567705,
-            'longitude': 11.653147
-        },
-        '_id': 'italien-suedtirol-schlerngebietgroeden',
-    },
-    {
-        'type': 'district',
-        'name': {
-            'de': 'Überetsch - Unterland',
-            'it': 'Oltradige - Bassa Atesina',
-            'de_st': 'Überetsch - Unterlond'
-        },
-        'region': {
-            'de': 'Südtirol',
-            'it': 'Alto Adige'
-        },
-        'country': {
-            'de': 'Italien',
-            'it': 'Italia'
-        },
-        'coordinates': {
-            'latitude': 46.345476,
-            'longitude': 11.296343
-        },
-        '_id': 'italien-suedtirol-ueberetschunterland',
-    },
-    {
-        'type': 'district',
-        'name': {
-            'de': 'Vinschgau',
-            'it': 'Val Venosta',
-            'de_st': 'Vinschgau'
-        },
-        'region': {
-            'de': 'Südtirol',
-            'it': 'Alto Adige'
-        },
-        'country': {
-            'de': 'Italien',
-            'it': 'Italia'
-        },
-        'coordinates': {
-            'latitude': 46.640144,
-            'longitude': 10.732828
-        },
-        '_id': 'italien-suedtirol-vinschgau',
-    },
-    {
-        'type': 'district',
-        'name': {
-            'de': 'Wipptal',
-            'it': 'Wipptal',
-            'de_st': 'Wipptol'
-        },
-        'region': {
-            'de': 'Südtirol',
-            'it': 'Alto Adige'
-        },
-        'country': {
-            'de': 'Italien',
-            'it': 'Italia'
-        },
-        'coordinates': {
-            'latitude': 46.89412,
-            'longitude': 11.463419
-        },
-        '_id': 'italien-suedtirol-wipptal',
-    }
-];
 
